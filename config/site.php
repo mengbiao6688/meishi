e59c00aafd5d07c68d01d1c4ff976216df69e172
mengbiao <542197679@qq.com>
2016-03-20 09:36:47 +0800

    修改网站标题、建立数据库迁移文件

--- config/site.php
+++ config/site.php
@@ -2,7 +2,7 @@
 
 return [
 
-	'title' => '网站标题',
+	'title' => '美食网',
 	'title_reverse' => true, //显示标题时，FALSE顺序为：主标题 - 子标题 - 子标题，设置为TRUE，则表示反转显示
 		'pagesize' => [
 			'common' => 25,
---
+++ database/migrations/2016_03_19_162518_create_meishi_tables.php
@@ -0,0 +1,63 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMeishiTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('meishi', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('gongyi',20)->comment = '工艺';
+            $table->string('kouwei',20)->comment = '口味';
+            $table->text('yongliao')->comment = '用料';
+            $table->text('zuofa')->comment = '做法';
+            $table->unsignedTinyInteger('special')->comment = '特别菜谱';
+            $table->unsignedTinyInteger('type')->comment = '国外菜肴，国内菜肴，家常菜肴';
+            $table->softDeletes();
+            $table->timestamps();
+        });
+
+        Schema::create('category', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name',20)->comment = '分类名称';
+            $table->unsignedInteger('parent_id')->comment = '上级分类';
+            $table->softDeletes();
+            $table->timestamps();
+        });
+
+        Schema::create('meishi_cover', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('meishi_id')->comment = '美食ID';
+            $table->unsignedInteger('cover_id')->comment = '图片ID';
+            $table->timestamps();
+        });
+
+        Schema::create('customer', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('username',20)->comment = '用户名';
+            $table->string('password',32)->comment = '密码';
+            $table->timestamp('lastlogin')->comment = '上次登录';
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('meishi');
+        Schema::drop('category');
+        Schema::drop('customer');
+    }
+}
